e1dc6d9d9bea19c88612cd6e64946514d87af66a
pengshiyu <1940607002@qq.com>
2021-02-09 13:51:57 +0800

    fix

--- test/test_history.py
+++ test/test_history.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from spider_admin_pro.model.history import HistoryModel
+from spider_admin_pro.model import ScheduleHistoryModel
 import unittest
 
 
 class HistoryTest(unittest.TestCase):
     def test_insert(self):
-        history = HistoryModel(project="project", spider="baidu", schedule_job_id="1", spider_job_id="1")
+        history = ScheduleHistoryModel(project="project", spider="baidu", schedule_job_id="1", spider_job_id="1")
         history.save()
 
     def test_query(self):
-        rows = HistoryModel.select().order_by(HistoryModel.create_time.desc()).paginate(1, 10).dicts()
+        rows = ScheduleHistoryModel.select().order_by(HistoryModel.create_time.desc()).paginate(1, 10).dicts()
 
         # rows = HistoryModel
         # print(list(rows))
