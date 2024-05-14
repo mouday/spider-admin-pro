bbfd55dd7370faec9d9868d9a64a4e4722005d16
dream2333 <vincentqng@gmail.com>
2024-05-14 15:07:03 +0800

    添加zstd, gzip, br, deflate压缩支持，对于大日志读取更加友好

--- spider_admin_pro/main.py
+++ spider_admin_pro/main.py
@@ -6,8 +6,10 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from spider_admin_pro.router import register_blueprint
 from spider_admin_pro.service import system_task_service
 from spider_admin_pro.utils.flask_ext.flask_app import FlaskApp
+from flask_compress import Compress
 
 app = FlaskApp(__name__, static_folder=None)
+Compress(app)
 CORS(app, supports_credentials=True)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
@@ -18,7 +20,7 @@ register_blueprint(app)
 @app.before_request
 def before_request():
     """跨域请求会出现options，直接返回即可"""
-    if request.method == 'OPTIONS':
+    if request.method == "OPTIONS":
         return make_response()
 
 
