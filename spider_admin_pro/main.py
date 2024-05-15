be3ef06942a2aac14c9026c0de742d8287ccdac5
dream2333 <vincentqng@gmail.com>
2024-05-15 17:50:45 +0800

    添加静态文件预压缩功能，在每次启动wgsi服务器时压缩静态文件

--- spider_admin_pro/main.py
+++ spider_admin_pro/main.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
+
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from spider_admin_pro.router import register_blueprint
 from spider_admin_pro.service import system_task_service
 from spider_admin_pro.utils.flask_ext.flask_app import FlaskApp
+from spider_admin_pro.utils.statics import compress_statics
+from spider_admin_pro.logger import logger
 from flask_compress import Compress
 from whitenoise import WhiteNoise
 import importlib.resources as R
 
+static_dir = str(R.files("spider_admin_pro") / "public")
+# 静态文件预压缩
+logger.info("开始静态文件预压缩")
+compress_statics(static_dir)
+logger.info("结束静态文件预压缩")
 app = FlaskApp(__name__, static_folder=None, template_folder=None)
-
-# 中间件、wsgi_app、静态文件处理
+# 为wsgi接口响应添加压缩功能
 Compress(app)
+# 跨域支持
 CORS(app, supports_credentials=True, max_age=6000)
-static_dir = R.files("spider_admin_pro") / "public"
-app.wsgi_app = WhiteNoise(ProxyFix(app.wsgi_app), root=str(static_dir), index_file=True)
-
+# 静态文件服务
+app.wsgi_app = WhiteNoise(ProxyFix(app.wsgi_app), root=static_dir, index_file=True)
 # 注册路由
 register_blueprint(app)
 
